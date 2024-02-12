504db4aa53284addb1e4a4678cf7a8ceb8c831c5
Shahzaib Ahmed <shahzaibahmed@gmail.com>
2024-02-12 19:38:05 +0500

    minor format currency

--- lib/src/utils/app_extensions.dart
+++ lib/src/utils/app_extensions.dart
@@ -4,13 +4,20 @@ extension NumberEx on num {
   String get toCurrency {
     // Convert the amount from øre to kroner
     double amountInKroner = this / 100;
+
+    return _formatToCurrency(amountInKroner);
+  }
+
+  String get toSimpleCurrency => _formatToCurrency(this);
+
+  String _formatToCurrency(num number) {
     final format = NumberFormat.currency(
       locale: 'da-DK',
       symbol: 'kr.',
       customPattern: '#,##0.00 ¤',
     );
 
-    final formattedAmount = format.format(amountInKroner);
+    final formattedAmount = format.format(number);
     return formattedAmount;
   }
 }
