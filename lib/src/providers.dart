1ca797a601887ac9e09dd5eecf963f620f7eafd3
Shahzaib Ahmed <shahzaibahmed@gmail.com>
2025-04-30 14:39:09 +0500

    feat: Remove graphql_flutter and update Firebase dependencies
    - Removed the `graphql_flutter` package from `pubspec.yaml`.
    - Removed the `graphQLClientProvider` from `providers.dart`.
    - Updated `firebase_core` to version `3.13.0`.
    - Updated `cloud_firestore` to version `5.6.7`.
    - Updated `cloud_functions` to version `5.5.0`.

--- lib/src/providers.dart
+++ lib/src/providers.dart
@@ -1,19 +1,7 @@
 import 'package:cloud_functions/cloud_functions.dart';
-import 'package:graphql_flutter/graphql_flutter.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:shared_preferences/shared_preferences.dart';
-
-final graphQLClientProvider = Provider<GraphQLClient>((ref) {
-  final HttpLink httpLink = HttpLink(
-    'https://graphql.ordrestyring.dk/graphql',
-    defaultHeaders: {
-      'Authorization': 'Bearer GdCt6bBGO23GZKsp',
-    },
-  );
-
-  return GraphQLClient(link: httpLink, cache: GraphQLCache());
-});
 
 final firestoreProvider = Provider<FirebaseFirestore>(
   (ref) => FirebaseFirestore.instance,
